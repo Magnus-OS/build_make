a84101e895c1f201012b85f73e198a6f2e351823
ReveRTX <rev9rt@gmail.com>
2022-07-11 13:15:47 +0000

    envsetup: Do not show missing file error on terminal
    Signed-off-by: ReveRTX <rev9rt@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -666,12 +666,6 @@ function lunch()
         # if we can't find a product, try to grab it off the LineageOS GitHub
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lineage/build/tools/roomservice.py $product
-        cd - > /dev/null
-        check_product $product
-    else
-        T=$(gettop)
-        cd $T > /dev/null
         vendor/lineage/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
